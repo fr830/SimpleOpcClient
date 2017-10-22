29ad1501aa62aab1ed67cfce7f5765015b57e003
Sasha654 <postdump@rambler.ru>
2017-10-22 11:02:41 +0300

    Add project files.

---
+++ SimpleOpcClient/MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace SimpleOpcClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbServerUrl = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnBrowseNodes = new System.Windows.Forms.Button();
+            this.tvBrowse = new System.Windows.Forms.TreeView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsSessionState = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tbReference = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbServerUrl
+            // 
+            this.tbServerUrl.BackColor = System.Drawing.SystemColors.Window;
+            this.tbServerUrl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbServerUrl.Location = new System.Drawing.Point(3, 3);
+            this.tbServerUrl.Name = "tbServerUrl";
+            this.tbServerUrl.Size = new System.Drawing.Size(368, 20);
+            this.tbServerUrl.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnConnect.Location = new System.Drawing.Point(3, 3);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(116, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDisconnect.Location = new System.Drawing.Point(247, 3);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(118, 23);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnBrowseNodes
+            // 
+            this.btnBrowseNodes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnBrowseNodes.Location = new System.Drawing.Point(125, 3);
+            this.btnBrowseNodes.Name = "btnBrowseNodes";
+            this.btnBrowseNodes.Size = new System.Drawing.Size(116, 23);
+            this.btnBrowseNodes.TabIndex = 3;
+            this.btnBrowseNodes.Text = "Browse nodes";
+            this.btnBrowseNodes.UseVisualStyleBackColor = true;
+            this.btnBrowseNodes.Click += new System.EventHandler(this.btnBrowseNodes_Click);
+            // 
+            // tvBrowse
+            // 
+            this.tvBrowse.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.tvBrowse.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvBrowse.Location = new System.Drawing.Point(3, 63);
+            this.tvBrowse.Name = "tvBrowse";
+            this.tvBrowse.Size = new System.Drawing.Size(368, 369);
+            this.tvBrowse.TabIndex = 4;
+            this.tvBrowse.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.tvBrowse_AfterExpand);
+            this.tvBrowse.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvBrowse_AfterSelect);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsSessionState});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 490);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(374, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tsSessionState
+            // 
+            this.tsSessionState.Name = "tsSessionState";
+            this.tsSessionState.Size = new System.Drawing.Size(33, 17);
+            this.tsSessionState.Text = "State";
+            // 
+            // tbReference
+            // 
+            this.tbReference.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbReference.Location = new System.Drawing.Point(3, 438);
+            this.tbReference.Multiline = true;
+            this.tbReference.Name = "tbReference";
+            this.tbReference.ReadOnly = true;
+            this.tbReference.Size = new System.Drawing.Size(368, 49);
+            this.tbReference.TabIndex = 6;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tbReference, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tbServerUrl, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tvBrowse, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(374, 490);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel2.Controls.Add(this.btnConnect, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnDisconnect, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnBrowseNodes, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 28);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(368, 29);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnConnect;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 512);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "MainForm";
+            this.Text = "<-SimpleOpcClient ->";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbServerUrl;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnBrowseNodes;
+        private System.Windows.Forms.TreeView tvBrowse;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TextBox tbReference;
+        private System.Windows.Forms.ToolStripStatusLabel tsSessionState;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    }
+}
+
---
+++ SimpleOpcClient/MainForm.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Windows.Forms;
+using Opc.Ua.Client;
+using Opc.Ua;
+
+namespace SimpleOpcClient
+{
+    public partial class MainForm : Form
+    {
+        private Session _session;
+        private ApplicationConfiguration _configuration;
+        private Browser _browser;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _configuration = new ApplicationConfiguration
+            {
+                ApplicationType = ApplicationType.Client,
+                CertificateValidator = new CertificateValidator(),
+                ClientConfiguration = new ClientConfiguration()
+            };
+
+            this.tbServerUrl.Text = "opc.tcp://localhost:4848";
+            tsSessionState.Text = "Disconnected";
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.btnDisconnect_Click(this, EventArgs.Empty);
+
+                EndpointDescription endpointDescription = SelectEndpoint(tbServerUrl.Text, false);
+                EndpointConfiguration endpointConfiguration = EndpointConfiguration.Create(null);
+                ConfiguredEndpoint endpoint = new ConfiguredEndpoint(null, endpointDescription, endpointConfiguration);
+                _session = Session.Create(_configuration, endpoint, false, true, Application.ProductName, 60000, null, null);
+
+                tsSessionState.Text = _session.Connected == false ? "Disconnected" : "Connected";
+            }
+            catch (ServiceResultException ex)
+            {
+                tsSessionState.Text = ex.Message;
+            }
+            catch (UriFormatException ex)
+            {
+                tsSessionState.Text = ex.Message;
+            }
+        }
+
+        private EndpointDescription SelectEndpoint(string discoveryUrl, bool useSecurity)
+        {
+            Uri uri = new Uri(discoveryUrl);
+            EndpointDescription selectedEndpoint = null;
+
+            using (DiscoveryClient client = DiscoveryClient.Create(uri))
+            {
+                EndpointDescriptionCollection endpoints = client.GetEndpoints(null);
+
+                for (int i = 0; i < endpoints.Count; i++)
+                {
+                    EndpointDescription endpoint = endpoints[i];
+
+                    if (endpoint.EndpointUrl.StartsWith(uri.Scheme))
+                    {
+                        if (useSecurity)
+                        {
+                            if (endpoint.SecurityMode == MessageSecurityMode.None)
+                                continue;
+                        }
+                        else
+                        {
+                            if (endpoint.SecurityMode != MessageSecurityMode.None)
+                                continue;
+                        }
+
+                        if (selectedEndpoint == null)
+                            selectedEndpoint = endpoint;
+
+                        if (endpoint.SecurityLevel > selectedEndpoint.SecurityLevel)
+                            selectedEndpoint = endpoint;
+                    }
+                }
+
+                if (selectedEndpoint == null && endpoints.Count > 0)
+                    selectedEndpoint = endpoints[0];
+                return selectedEndpoint;
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (_session != null)
+            {
+                _session.Close(10000);
+                tsSessionState.Text = _session.Connected == false ? "Disconnected" : "Connected";
+                _session = null;
+
+                tvBrowse.Nodes.Clear();
+                tbReference.Clear();
+            }
+        }
+
+        private void btnBrowseNodes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NodeId rootNodeId = ObjectIds.ObjectsFolder;
+                INode node = _session.NodeCache.Find(rootNodeId);
+
+                _browser = new Browser(_session)
+                {
+                    BrowseDirection = BrowseDirection.Forward,
+                    ContinueUntilDone = true,
+                    ReferenceTypeId = ReferenceTypeIds.HierarchicalReferences
+                };
+
+                TreeNode treeNode = tvBrowse.Nodes.Add(node.ToString());
+                treeNode.Tag = new ReferenceDescription { NodeId = node.NodeId };
+                treeNode.Nodes.Add(string.Empty);   //To display the plus/minus button
+                treeNode.Expand();
+            }
+            catch (NullReferenceException)
+            {
+                tsSessionState.Text = "Connection not established.";
+            }
+        }
+
+        private void tvBrowse_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            ReferenceDescription reference = (ReferenceDescription)e.Node.Tag;
+            NodeId node = (NodeId)reference.NodeId;
+
+            Browser browser = new Browser(_session)
+            {
+                ContinueUntilDone = true,
+                BrowseDirection = BrowseDirection.Forward,
+                ReferenceTypeId = ReferenceTypeIds.References
+            };
+
+            ReferenceDescriptionCollection rdc = browser.Browse(node);
+
+            string nodeType = rdc[0].BrowseName.Name;
+            string id = node.Identifier.ToString();
+            tbReference.Text = string.Format("Identifier: {1}\r\nHasTypeDefinition: {0}", nodeType, id);
+        }
+
+        private void tvBrowse_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            ReferenceDescription rd = e.Node.Tag as ReferenceDescription;
+            NodeId root = (NodeId)rd.NodeId;
+
+            CreateTreeView(root, e.Node);
+        }
+
+        private void CreateTreeView(NodeId rootNodeId, TreeNode treeNode)
+        {
+            ReferenceDescriptionCollection rdcOrg = _browser.Browse(rootNodeId);
+
+            foreach (ReferenceDescription referenceDes in rdcOrg)
+            {
+                TreeNode tn = treeNode.Nodes.Add(referenceDes.DisplayName.Text);
+                tn.Tag = referenceDes;
+                tn.Nodes.Add(string.Empty);
+            }
+        }
+    }
+}
